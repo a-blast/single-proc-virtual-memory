b80d02071f378a8b9cd68fd9b1c93fb9efa4226b
Austin <austin@growatomata.com>
2019-02-27 14:15:35 -0700

    Sets the kernel page table in the MMU
    Sets it in page 2
    not implemented: page table manager, allocator->MMU connection,
    process->allocator connection.

--- main.cpp
+++ main.cpp
@@ -12,6 +12,18 @@
 #include <iostream>
 #include <memory>
 
+void setPageTable(mem::MMU &vm, mem::Addr pt_addr){
+  mem::PageTable kernel_page_table;  // local copy of page table to build, initialized to 0
+  mem::Addr num_pages = vm.get_frame_count();  // size of physical memory
+  // Build page table entries
+  for (mem::Addr i = 0; i < num_pages; ++i) {
+    kernel_page_table.at(i) = 
+      (i << mem::kPageSizeBits);
+  }
+  // Write page table to memory
+  vm.movb(pt_addr, &kernel_page_table, mem::kPageTableSizeBytes);
+}
+
 int main(int argc, char** argv) {
 
   if (argc != 2) {
@@ -22,6 +34,7 @@ int main(int argc, char** argv) {
   // 1. Create a instance of MMU w/ 128 (0x80) page frames
   mem::MMU* memory = new mem::MMU(128);
 
+  setPageTable(*memory, mem::kPageSize);
   // initialize an allocator that points to the MMU physical mem instance
   MemoryAllocator* allocator = new MemoryAllocator(128, memory);
 
